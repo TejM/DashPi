204fa62bf542a6a71ae7f7ce5f44c44e10a90361
“Tejas <tejas.mallela@gmail.com>
2020-09-22 18:57:56 -0500

    DAS-10 Connect Django and React

---
+++ back_end/front_end/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* This is placekeeper code, to verify that django is indeed rendering
+      the react front end */}
+      <div className='center-column'>
+        <div className='item-row'>
+          <span>One</span>
+        </div>
+
+        <div className='item-row'>
+          <span>Two</span>
+        </div>
+
+        <div className='item-row'>
+          <span>Three</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
