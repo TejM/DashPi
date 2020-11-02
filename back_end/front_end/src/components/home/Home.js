4413fd189cc274b09c5dce70066e0c6734c3ba5e
cfinnell92 <57780687+cfinnell92@users.noreply.github.com>
2020-11-01 20:44:56 -0800

    DAS-18 Add Navigation Buttons (#10)

--- back_end/front_end/src/components/home/Home.js
+++ back_end/front_end/src/components/home/Home.js
@@ -1,21 +1,31 @@
 import React from "react";
 import camera from "./../../images/camera.svg";
-import react from "./../../images/react.svg";
+import can from "./../../images/can.svg";
+import settings from "./../../images/settings.svg";
 import { IconButton } from "@material-ui/core";
 
 const home = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={react} className="App-logo" alt="react" />
         <p>{Date().toLocaleString()}</p>
         <div>
           <IconButton color="primary" aria-label="camera view" href="/camera">
-            <img src={camera} className="Camera-logo" alt="camera" />
+            <img src={camera} className="App-logo" alt="camera" />
+          </IconButton>
+          <IconButton color="primary" aria-label="can view" href="/CAN">
+            <img src={can} className="App-logo" alt="can" />
+          </IconButton>
+          <IconButton
+            color="primary"
+            aria-label="settings view"
+            href="/settings"
+          >
+            <img src={settings} className="App-logo" alt="settings" />
           </IconButton>
         </div>
         <div>
-          Icon from{" "}
+          Icons from{" "}
           <a href="https://www.flaticon.com/" title="Flaticon">
             {" "}
             www.flaticon.com
--- back_end/front_end/src/components/navigation/Navigation.js
+++ back_end/front_end/src/components/navigation/Navigation.js
@@ -1,13 +1,22 @@
 import React from "react";
-
-import { NavLink } from "react-router-dom";
+import home from "./../../images/home.svg";
+import { useHistory } from "react-router-dom";
+import { IconButton } from "@material-ui/core";
+import "./Navigation.css";
 
 const Navigation = () => {
+  let history = useHistory();
   return (
     <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/CAN">CAN</NavLink>
-      <NavLink to="/settings">Settings</NavLink>
+      <IconButton color="primary" aria-label="home view" href="/">
+        <img src={home} className="App-logo" alt="home" />
+      </IconButton>
+      <button class="button back-button" onClick={() => history.goBack()}>
+        Back
+      </button>
+      <button class="button forward-button" onClick={() => history.goForward()}>
+        Forward
+      </button>
     </div>
   );
 };
